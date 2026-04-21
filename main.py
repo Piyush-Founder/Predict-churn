3d2789a062fada4863a12bd2f3842f274b092c11
Piyush Shukla <serverhirosima5@gmail.com>
2026-04-21 13:05:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent
 
-model = joblib.load(BASE_DIR / "xgboost_churn_model.pkl")
-feature_columns = joblib.load(BASE_DIR / "feature_columns.pkl")
+model = joblib.load("xgboost_churn_model.pkl")
+feature_columns = joblib.load("feature_columns.pkl")
 
 app.add_middleware(
     CORSMiddleware,
